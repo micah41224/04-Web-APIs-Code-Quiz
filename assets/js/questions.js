ce9258b64033881b04343d0c95345e211d7d26a4
Mike <micah41224@gmail.com>
2021-07-19 16:55:52 -0400

    First commit with very basic HTML CSS and java questions built out

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT include:",
+    answer: "alerts",
+    options: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed within _____.",
+    answer: "parentheses",
+    options: [
+      "quotes",
+      "curly brackets",
+      "parentheses",
+      "square brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store _____.",
+    answer: "all of the above",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answer: "quotes",
+    options: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parentheses"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "console.log",
+    options: [
+      "JavaScript",
+      "terminal/bash",
+      "for loops",
+      "console.log"
+    ]
+  },
+]
