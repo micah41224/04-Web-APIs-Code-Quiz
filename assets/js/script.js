6ffac41d6ab9849591228b050f8719596b47d3f7
Mike <micah41224@gmail.com>
2021-07-19 18:36:10 -0400

    Redesigned the question and answer format in the javascript to try a different build approach

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,101 @@
+const startQuiz = document.getElementById("startQuiz");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choice1 = document.getElementById("1")
+const choice2 = document.getElementById("2")
+const choice3 = document.getElementById("3")
+const choice4 = document.getElementById("4")
+
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT include:",
+    answer: "choice3",
+    choice1 : "strings",
+    choice2 : "booleans",
+    choice3 : "alerts",
+    choice4 : "numbers"
+    
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed within _____.",
+    answer: "choice3",
+    choice1 :  "quotes",
+    choice2 :  "curly brackets",
+    choice3 :  "parentheses",
+    choice4 :  "square brackets"
+    
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store _____.",
+    answer: "choice4",
+    choice1 : "numbers and strings",
+    choice2 : "other arrays",
+    choice3 : "booleans",
+    choice4 : "all of the above"
+    
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answer: "choice3",
+    choice1 : "commas",
+    choice2 : "curly brackets",
+    choice3 : "quotes",
+    choice4 : "parentheses"
+    
+  },
+  {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "choice4",
+    choice1 : "JavaScript",
+    choice2 : "terminal/bash",
+    choice3 : "for loops",
+    choice4 : "console.log"
+    
+  },
+]
+
+
+
+getQuiz(
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 setTimeout(
     () => {
         console.log('Hello after 4 seconds');
     },
     4 * 1000
 );
+*/
